ea3aae87092d1ffa6821b1e063add39818a41465
laxman006 <kadarisailaxman@gmail.com>
2024-09-21 20:35:38 +0530

    Restored the bootstrap version using hash code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -6,8 +6,8 @@ const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        {/* <Navbar.Brand href="/">Health Care</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
+        {/* <Navbar.Brand href="/">Health Care</Navbar.Brand> */}
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <LinkContainer to="/">
